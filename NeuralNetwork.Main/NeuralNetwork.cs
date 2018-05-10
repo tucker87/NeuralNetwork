7e3005ff7f0e8cf40c39163a05b2a62fa7f1c7ca
Jason Tucker <jtgeek@gmail.com>
2018-05-10 09:49:35 -0400

    Initial

---
+++ NeuralNetwork.Main/NeuralNetwork.cs
@@ -0,0 +1,75 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+
+// ReSharper disable InconsistentNaming
+
+namespace NeuralNetworkNS.Main
+{
+    public class NeuralNetwork
+    {
+        private readonly MatrixBuilder<double> M = Matrix<double>.Build;
+        public Matrix<double> Weights_IH { get; set; }
+        public Matrix<double> Weights_HO { get; set; }
+        public Matrix<double> Bias_H { get; set; }
+        public Matrix<double> Bias_O { get; set; }
+        public double LearningRate { get; set; } = 0.1;
+
+        public NeuralNetwork(int inputNodeCount, int hiddenNodeCount, int outputNodeCount)
+        {
+            Weights_IH = M.Random(hiddenNodeCount, inputNodeCount);
+            Weights_HO = M.Random(outputNodeCount, hiddenNodeCount);
+            Bias_H = M.Random(hiddenNodeCount, 1);
+            Bias_O = M.Random(outputNodeCount, 1);
+        }
+
+        public double[] Predict(double[] inputsArray)
+        {
+            var inputs = M.DenseOfColumnArrays(inputsArray);
+            var hiddens = CalculateHiddenLayer(inputs);
+            var output = CalculateOutputLayer(hiddens);
+
+            return output.ToRowMajorArray();
+        }
+
+        public void Train(double[] inputsArray, double[] targetsArray)
+        {
+            var inputs = M.DenseOfColumnArrays(inputsArray);
+            var hiddens = CalculateHiddenLayer(inputs);
+            var outputs = CalculateOutputLayer(hiddens);
+
+            var targets = M.DenseOfColumnArrays(targetsArray);
+            var outputErrors = targets.Subtract(outputs);
+
+            var outputGradients = outputs.Map(MathNet.Numerics.SpecialFunctions.Logit) * outputErrors * LearningRate;
+
+            var weightHODeltas = outputGradients * hiddens.Transpose();
+
+            Weights_HO = Weights_HO + weightHODeltas;
+            Bias_O = Bias_O + outputGradients;
+
+            var hiddenErrors = Weights_HO.Transpose() * outputErrors;
+
+            var hiddenGradients = hiddens.Map(MathNet.Numerics.SpecialFunctions.Logit);
+            hiddenGradients = hiddenGradients * hiddenErrors;
+            hiddenGradients = hiddenGradients * LearningRate;
+
+            var wightIHDeltas = hiddenGradients * inputs.Transpose();
+
+            Weights_IH = Weights_IH + wightIHDeltas;
+            Bias_H = Bias_H + hiddenGradients;
+        }
+
+        private Matrix<double> CalculateHiddenLayer(Matrix<double> inputs)
+        {
+            var hiddens = Weights_IH * inputs + Bias_H;
+            hiddens = hiddens.Map(MathNet.Numerics.SpecialFunctions.Logistic);
+            return hiddens;
+        }
+
+        private Matrix<double> CalculateOutputLayer(Matrix<double> hiddens)
+        {
+            var output = Weights_HO * hiddens + Bias_O;
+            output = output.Map(MathNet.Numerics.SpecialFunctions.Logistic);
+            return output;
+        }
+    }
+}
---
+++ NeuralNetwork/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NeuralNetworkNS.Main;
+
+namespace NeuralNetworkNS.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            var nn = new NeuralNetwork(2, 2, 1);
+
+            var result = nn.Predict(new double[] {0, 1});
+            System.Console.WriteLine($"Before: {result}");
+
+            var data = new List<(double[] inputs, double[] outputs)>
+            {
+                (new double[] { 0, 0 }, new double[]{0}),
+                (new double[] { 1, 1 }, new double[]{0}),
+                (new double[] { 0, 1 }, new double[]{1}),
+                (new double[] { 1, 0 }, new double[]{1})
+            };
+
+            foreach (var _ in Enumerable.Range(0, 99))
+            {
+                var index = random.Next(0, 4);
+                var trainingData = data[index];
+                nn.Train(trainingData.inputs, trainingData.outputs);
+            }
+
+            result = nn.Predict(new double[] { 0, 1 });
+            System.Console.WriteLine($"After: {result}");
+        }
+    }
+}
