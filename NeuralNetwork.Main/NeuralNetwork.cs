9183b0fb942e6bb03b5520e033d6ad1bfa2f2727
Jason Tucker <jtgeek@gmail.com>
2018-05-11 21:15:16 -0400

    found the missing line

--- NeuralNetwork.Main/NeuralNetwork.cs
+++ NeuralNetwork.Main/NeuralNetwork.cs
@@ -43,7 +43,8 @@ namespace NeuralNetworkNS.Main
             var (inputs, hiddens, outputs) = FeedForward(inputsArray);
             var outputErrors = M.DenseOfColumnArrays(targetsArray) - outputs;
             AdjustLayer(outputErrors, outputs, hiddens, ref Weights_HO, ref Bias_O);
-            AdjustLayer(outputErrors, hiddens, inputs, ref Weights_IH, ref Bias_H);
+            var hiddenErrors = Weights_HO.Transpose() * outputErrors;
+            AdjustLayer(hiddenErrors, hiddens, inputs, ref Weights_IH, ref Bias_H);
         }
 
         private void AdjustLayer(Matrix<double> outputErrors, Matrix<double> outputs, Matrix<double> hiddens, ref Matrix<double> weights, ref Matrix<double> bias)
